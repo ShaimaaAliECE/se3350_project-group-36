81990ab39ee0e4385930e225c137283e9f5d9639
Rohan <rkamraly@uwo.ca>
2022-02-04 13:45:22 -0500

    barebones index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const express = require("express");
 
-const API_PORT = process
+const API_PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.post("/login", (req, res) => {
+    res.json({
+        message: "Hello API"
+    });
+});
+
+app.listen(API_PORT, () => {
+    console.log(`listening on port ${API_PORT}`);
+});
