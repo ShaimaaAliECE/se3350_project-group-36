7d6d04136c805200581e342f1d2cfdc8379c2996
Rohan <rkamraly@uwo.ca>
2022-02-08 19:26:36 -0500

    Attempting to make mergeSort seperate file

---
+++ static/merge-sort.js
@@ -0,0 +1,32 @@
+function merge(left, right) {
+    let workingArray = [];
+
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            workingArray.push(left.shift()); 
+        } else {
+            workingArray.push(right.shift());
+        }
+    }
+
+    return [ ...workingArray, ...left, ...right ];
+}
+
+function mergeSort(givenArray) {
+    const half = givenArray.length / 2;
+
+    if(givenArray.length < 2){
+        return givenArray; 
+    }
+
+    const left = givenArray.splice(0, half);
+    return merge(mergeSort(left),mergeSort(givenArray));
+}
+
+function finalSort(finalArray) {
+    return mergeSort(finalArray);
+}
+
+function test() {
+    return "Hello World";
+}
