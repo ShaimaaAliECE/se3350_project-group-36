3b8382e4f58cf18220e7f69be850b1ac87c579e2
Rohan <rkamraly@uwo.ca>
2022-02-15 17:55:38 -0500

    Added skeleton level 2 and 3 pages, and added index.js links

--- index.js
+++ index.js
@@ -17,22 +17,26 @@ app.post("/login", (req, res) => {
 
 app.get("/main-menu", (req, res) => {
     res.sendFile( __dirname + "/static/" + "index.html" );
-
 });
 
 app.get("/algorithm-select", (req, res) => {
     res.sendFile( __dirname + "/static/" + "/pages/" + "algorithm-select.html" );
-
 });
 
 app.get("/level-select", (req, res) => {
     res.sendFile( __dirname + "/static/" + "/pages/" + "level-select.html" );
-
 });
 
 app.get("/level-1", (req, res) => {
     res.sendFile( __dirname + "/static/" + "/pages/" + "/levels/" + "level-1.html" );
+});
 
+app.get("/level-2", (req, res) => {
+    res.sendFile( __dirname + "/static/" + "/pages/" + "/levels/" + "level-2.html" );
+});
+
+app.get("/level-3", (req, res) => {
+    res.sendFile( __dirname + "/static/" + "/pages/" + "/levels/" + "level-3.html" );
 });
 
 app.listen(80, () => {
