9a8b18a7062eea808ef609ea1577adfd4cc9b99e
emachej <emachej@uwo.ca>
2022-02-18 16:37:13 -0500

    WIP step tracking in mergesort file

--- static/scripts/merge-sort.js
+++ static/scripts/merge-sort.js
@@ -1,4 +1,6 @@
 function merge(left, right) {
+    i=0;
+    tracker1 = new Array(20);
     let workingArray = [];
 
     while (left.length && right.length) {
@@ -8,11 +10,18 @@ function merge(left, right) {
             workingArray.push(right.shift());
         }
     }
+    //step tracking
+    i++;
+    tracker1[i] = [ ...workingArray, ...left, ...right ];
+    document.write("<br>sugma: ", tracker1[i]);
+
     document.write("<br>merge: working: ", workingArray, " + left: ", left, " + right: ", right);
     return [ ...workingArray, ...left, ...right ];
 }
 
 function mergeSort(givenArray) {
+    i = 0;
+    tracker2 = new Array(20);
     const half = givenArray.length / 2;
 
     if(givenArray.length < 2){
@@ -20,6 +29,11 @@ function mergeSort(givenArray) {
     }
 
     const left = givenArray.splice(0, half);
+    //step tracking
+    i++;
+    tracker2[i] = left;
+    document.write("<br>ligma: ", tracker2[i]);
+    
     document.write("<br>mergesort: ", left, " + ", givenArray);
     return merge(mergeSort(left),mergeSort(givenArray));
 }
